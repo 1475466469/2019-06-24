f62be0f98ee1efe4b564f6c8941c4b8b32953d85
Administrator <Administrator@DESKTOP-4SMBBJM>
2019-06-24 16:12:12 +0800

    添加项目文件。

---
+++ Single/Controllers/SingleController.cs
@@ -0,0 +1,24 @@
+﻿using Single.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Single.Controllers
+{
+    public class SingleController : Controller
+    {
+        // GET: Single
+        public ActionResult Index()
+        {
+           ViewBag.ClientName = "聊客-" + Guid.NewGuid().ToString("N");
+        
+            var onLineUserList = MyHub.OnLineUsers.Select(u => new SelectListItem() { Text = u.Value, Value = u.Key }).ToList();
+           onLineUserList.Insert(0, new SelectListItem() { Text = "-所有人-", Value = "" });
+            ViewBag.OnLineUsers = onLineUserList;
+            return View();
+        }
+
+    }
+}
---
+++ Single/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Single.Startup))]
+namespace Single
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            ConfigureAuth(app);
+
+
+        }
+    }
+}
